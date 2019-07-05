6039a6ae6bb7697f6022ef898f0a2ffce0d01d53
雪猫 <SnowCait@users.noreply.github.com>
2019-07-06 08:08:34 +0900

    Update controller.php

--- framework/controller.php
+++ framework/controller.php
@@ -5,13 +5,19 @@ namespace framework;
 
 class Controller
 {
-    public function html(array $data): string
+    public function html(array $data): void
     {
         extract($data);
         $main = include "path/to/view/{$controller}/{$action}.php";
         $header = include "path/to/view/header.php";
         $footer = include "path/to/view/footer.php";
         $template = include "path/to/view/template.php";
-        return $template;
+        echo $template;
+    }
+    
+    public funcation json(array $data): void
+    {
+        header('Content-Type', 'application/json');
+        echo json_encode($data);
     }
 }
