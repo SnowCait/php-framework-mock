c1c7d68dd11a6aaa522f6cf32d93d99b5ed2e741
雪猫 <SnowCait@users.noreply.github.com>
2019-07-06 08:00:32 +0900

    Create template.php

---
+++ application/view/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <?= $header ?>
+        </header>
+        <main>
+            <?= $main ?>
+        </main>
+        <footer>
+            <?= $footer ?>
+        </footer>
+    </body>
+</html>
