b3e38c273e7cdab7b096ff23fd5f4dfbe032f559
雪猫 <SnowCait@users.noreply.github.com>
2019-07-06 05:06:21 +0900

    Create api.php

---
+++ test/api.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace test;
+
+use PHPUnit\Framework\TestCase;
+
+class Api extends TestCase
+{
+    /**
+     * @test
+     */
+    public function Hello()
+    {
+        $this->markTestIncomplete();
+    }
+}
